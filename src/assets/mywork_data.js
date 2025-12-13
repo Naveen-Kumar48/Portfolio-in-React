23d71eeb002f812e7e3368aeb8f389aa6008cd00
Naveen Kumar <kumarnaveen897941@gmail.com>
2025-12-13 17:14:21 +0530

    removed the typed js from contack page

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,22 +1,21 @@
-import project1_img from "../assets/project_img1.png"
-import project2_img from "../assets/project_img2.png"
-import project3_img from "../assets/project_img3.png"
+import project1_img from "../assets/project_img1.png";
+import project2_img from "../assets/project_img2.png";
+import project3_img from "../assets/project_img3.png";
 const mywork_data = [
-    {
-        w_no: 1,
-        w_name: "Web design",
-        w_img: project1_img
-    },
-    {
-        w_no: 2,
-        w_name: "Web design",
-        w_img: project2_img
-    },
-    {
-        w_no: 3,
-        w_name: "Web design",
-        w_img: project3_img
-    }
-    
-]
+  {
+    w_no: 1,
+    w_name: "Todo List",
+    w_img: project1_img,
+  },
+  {
+    w_no: 2,
+    w_name: "Whatsapp Chats clone",
+    w_img: project2_img,
+  },
+  {
+    w_no: 3,
+    w_name: "Fakestore",
+    w_img: project3_img,
+  },
+];
 export default mywork_data;
